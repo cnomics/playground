9bddb167c4bc4562cac45c0c4c0f72380944ab6c
cnomics <cnomics@icloud.com>
2020-01-24 08:36:16 +0100

    Switch fixed to random delay.

--- odd_minutes.py
+++ odd_minutes.py
@@ -8,21 +8,20 @@ minutes = []
 for i in range(1, 60, 2):
     minutes.append(i)
 
-right_this_minute = datetime.today().minute
-
-print(right_this_minute)
-
 count = 1
 
-while count <= 5:
+for i in range(5):
+
+    right_this_minute = datetime.today().minute
+    
     if right_this_minute in minutes:
         print("This is an odd minute.")
-        time.sleep(60)
+        time.sleep(random.randint(1,60))
         count+=1
 
     else:
         print("Not an odd minute")
-        time.sleep(60)
+        time.sleep(random.randint(1,60))
         count+=1
 
 
