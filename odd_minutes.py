4586cea2712daa24488b3a4d0f5f8e345be2f685
cnomics <cnomics@icloud.com>
2020-01-24 08:24:00 +0100

    Add loop and delay

--- odd_minutes.py
+++ odd_minutes.py
@@ -1,21 +1,31 @@
 from datetime import datetime
+import time
+import random
+
 
 minutes = []
 
 for i in range(1, 60, 2):
     minutes.append(i)
 
-print(minutes)
-
 right_this_minute = datetime.today().minute
 
-if right_this_minute in minutes:
-    print("This is an odd minute.")
+print(right_this_minute)
 
-else:
-    print("Not an odd minute")
+count = 1
 
-    
-    
+while count <= 5:
+    if right_this_minute in minutes:
+        print("This is an odd minute.")
+        time.sleep(60)
+        count+=1
+
+    else:
+        print("Not an odd minute")
+        time.sleep(60)
+        count+=1
+
+
+
 
 
